0f86fcf0e79d7321e740b1a4f103c2abd9df3a1f
Davi Borges <davifelipe_300@hotmail.com>
2023-04-15 17:27:22 -0300

    feat: cria as classes de heranca

---
+++ ClubeLeitura.ConsoleApp/Compartilhado/RepositorioBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeLeitura.ConsoleApp.Compartilhado
+{
+    internal class RepositorioBase
+    {
+        internal int Id { get; set; } = 4;
+        internal List<object> _listaRegistro = new List<object>();
+        internal void IncrementarId()
+        {
+            this.Id++;
+        }
+    }
+}
---
+++ ClubeLeitura.ConsoleApp/Compartilhado/TelaBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeLeitura.ConsoleApp.Compartilhado
+{
+    internal class TelaBase
+    {
+        public int Id = 3;
+
+    }
+}
